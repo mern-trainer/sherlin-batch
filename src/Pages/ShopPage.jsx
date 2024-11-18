65656d17a264d7c6f47e2838b52e597352f86e67
MERN TRAINER <futuralabs.mern@gmail.com>
2024-11-18 11:25:08 +0530

    product listing

--- src/Pages/ShopPage.jsx
+++ src/Pages/ShopPage.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { FaStar } from 'react-icons/fa'
 
 const ShopPage = () => {
 
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]) // state for storing products
     
     const getProducts = async () => {
         try {
@@ -14,20 +16,39 @@ const ShopPage = () => {
         }
     }
 
-    useEffect(() => {
+    useEffect(() => { 
         getProducts()
     }, [])
 
     return (
-        <div>
-            {
-                products.map(product => {
-                    return <div key={product.id}>
-                        { product.title }
-                    </div>
-                })
-            }
-        </div>
+        <div className='d-flex justify-content-center gap-2 flex-wrap mt-2'>
+                {
+                    products.map(product => {
+                        
+                        const discountPrice = product.price - (product.price * product.discountPercentage / 100)
+
+                        return <div key={product.id} className='bg-light p-1' style={{width: "15rem"}}>
+                            <div>
+                                <img src={product.images[0]} alt={product.title} style={{aspectRatio: 1/1, objectFit: "contain", width: "15rem"}}/>
+                            </div>
+                            <div>
+                                <h5 className="text-truncate text-center">{product.title}</h5>
+                                <div className='d-flex justify-content-between'>
+                                    <div className='d-flex align-items-center gap-1'>
+                                        <div><FaStar /></div>
+                                        <div>{product.rating}</div>
+                                    </div>
+                                    <div>
+                                        <s>${product.price}</s>
+                                        <b>${discountPrice.toFixed(2)}</b>
+                                    </div>
+                                </div>
+                                <button className='btn btn-success w-100 mt-2'>Add To Cart</button>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
     )
 }
 
